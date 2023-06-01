0f63aad3ab80f0e6fc39c11d02347b7f68117249
santiago hurtado <sahurtaro@gmail.com>
2023-06-01 16:43:49 -0500

    Día 10. Decorador Input

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  template: `<button [ngStyle]="{ 'background-color': color }">
+    {{ label }}
+  </button>`,
+  styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
-
+export class ButtonComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() color!: string;
+  @Input() label!: string;
+  @Input() selection!: string;
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes ->', changes);
+  }
+  ngOnInit(): void {
+    console.log('OnInit');
+  }
+  ngOnDestroy(): void {
+    console.log('Destroy');
+  }
 }
